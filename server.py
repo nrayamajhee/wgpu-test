50ea274fbecad87bd5cb98d6445f3565ed23d07d
nrayamajhee <nishan@rayamajhee.com>
2022-07-06 00:27:04 -0700

    Implement a very rudimentary hot reload to both client and server side

--- server.py
+++ server.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python3
+
+from http.cookies import SimpleCookie
 from http.server import HTTPServer, SimpleHTTPRequestHandler, test
 import sys
 
+session = SimpleCookie()
+session["state"] = "latest"
+
 class RequestHandler(SimpleHTTPRequestHandler):
+    def respond(self, message):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(bytes(message, "utf8"))
+
+    def send_error(self, code, message):
+        if self.path == "/check-state":
+            self.respond(session["state"].value)
+        if self.path == "/mark-stale":
+            session["state"] = "stale"
+            self.respond("DONE!")
+        if self.path == "/reset-stale":
+            session["state"] = "latest"
+            self.respond("DONE!")
+
     def end_headers(self):
         self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
         self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
